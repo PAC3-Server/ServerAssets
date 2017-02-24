811278ca648465a5da4bc4797d696d1175f3e47a
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-24 02:48:09 +0100

    add sounds for death types

--- lua/autorun/server/resources.lua
+++ lua/autorun/server/resources.lua
@@ -1,3 +1,15 @@
 for _, name in ipairs((file.Find("resource/fonts/*", "GAME"))) do
 	resource.AddFile("resource/fonts/" .. name)
 end
+
+local function add_dir(dir)
+	local files, dirs = file.Find(dir .. "*", "GAME")
+	for _, name in ipairs(files) do
+		resource.AddFile(dir .. name)
+	end
+	for _, name in ipairs(dirs) do
+		add_dir(dir .. name .. "/")
+	end
+end
+
+add_dir("sound/pac_server/")
